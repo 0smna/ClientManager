a0202e728ff897e6f891e7d8d72c714bfee73ad5
Kenneth-Villaplana <77358778+Kenneth-Villaplana@users.noreply.github.com>
2026-03-01 21:04:49 -0600

    Todo 2

--- ClientManager/obj/Debug/net10.0/ClientManager.AssemblyInfo.cs
+++ ClientManager/obj/Debug/net10.0/ClientManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClientManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a174f7d5cb34a68ac76cce449425140488bae8eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+948449cdf1e937e5de058a96de4e9859b73da282")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClientManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClientManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
